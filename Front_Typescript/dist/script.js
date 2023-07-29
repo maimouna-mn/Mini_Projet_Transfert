f75744ea2661b4fc028325104ac2e6abde0f569e
maymouna <maymounandiaye55@gmail.com>
2023-07-29 21:36:46 +0000

    plus2

--- Front_Typescript/dist/script.js
+++ Front_Typescript/dist/script.js
@@ -1,24 +1,4 @@
 var _a, _b, _c;
-(_a = document.getElementById('fournisseur')) === null || _a === void 0 ? void 0 : _a.addEventListener('change', () => {
-    const selectElement = document.getElementById('fournisseur');
-    const transactionTitles = document.querySelectorAll('.transaction-title');
-    const selectedFournisseur = selectElement.value;
-    transactionTitles.forEach(transactionTitle => {
-        transactionTitle.classList.remove('om', 'wave', 'wari', 'cb');
-        if (selectedFournisseur === 'om') {
-            transactionTitle.classList.add('om');
-        }
-        else if (selectedFournisseur === 'wave') {
-            transactionTitle.classList.add('wave');
-        }
-        else if (selectedFournisseur === 'wari') {
-            transactionTitle.classList.add('wari');
-        }
-        else if (selectedFournisseur === 'cb') {
-            transactionTitle.classList.add('cb');
-        }
-    });
-});
 function transfert() {
     const montant = document.getElementById('montant').value;
     const expediteur = document.getElementById('expediteur').value;
@@ -37,6 +17,7 @@ function transfert() {
     })
         .then(response => response.json())
         .then((result) => {
+        var _a, _b;
         console.log(result.message);
         if (result.codeRetrait) {
             console.log('Code de retrait: ' + result.codeRetrait);
@@ -47,12 +28,16 @@ function transfert() {
         if (result.beneficiaire) {
             document.getElementById('destinataire_nom').value = result.beneficiaire.nom;
         }
+        if (result.message.includes('Transfert effectué avec succès.')) {
+            const message = `Le transfert effectué par ${(_a = result.emetteur) === null || _a === void 0 ? void 0 : _a.nom} vers ${(_b = result.beneficiaire) === null || _b === void 0 ? void 0 : _b.nom} est un succès.`;
+            notification(message);
+        }
     })
         .catch(error => console.error('Erreur lors du transfert:', error));
 }
 const boutonValider = document.querySelector('.btn-primary');
 boutonValider === null || boutonValider === void 0 ? void 0 : boutonValider.addEventListener('click', transfert);
-(_b = document.getElementById('expediteur')) === null || _b === void 0 ? void 0 : _b.addEventListener('change', () => {
+(_a = document.getElementById('expediteur')) === null || _a === void 0 ? void 0 : _a.addEventListener('change', () => {
     const expediteur = document.getElementById('expediteur').value;
     fetch('http://127.0.0.1:8000/api/getclient/' + expediteur)
         .then(response => response.json())
@@ -66,7 +51,7 @@ boutonValider === null || boutonValider === void 0 ? void 0 : boutonValider.addE
     })
         .catch(error => console.error('Erreur lors de la récupération du nom de l\'expéditeur:', error));
 });
-(_c = document.getElementById('destinataire')) === null || _c === void 0 ? void 0 : _c.addEventListener('change', () => {
+(_b = document.getElementById('destinataire')) === null || _b === void 0 ? void 0 : _b.addEventListener('change', () => {
     const destinataire = document.getElementById('destinataire').value;
     fetch('http://127.0.0.1:8000/api/getclient/' + destinataire)
         .then(response => response.json())
@@ -79,4 +64,40 @@ boutonValider === null || boutonValider === void 0 ? void 0 : boutonValider.addE
         }
     })
         .catch(error => console.error('Erreur lors de la récupération du nom du destinataire:', error));
+});
+function showNotification(message) {
+    console.log('showNotification function called.');
+    if ('Notification' in window && Notification.permission === 'granted') {
+        const notification = new Notification(message);
+    }
+}
+let container = document.querySelector('.container');
+function notification(text) {
+    let notifi = document.createElement("div");
+    notifi.textContent = text;
+    notifi.classList.add("notification");
+    container.appendChild(notifi);
+    setTimeout(() => {
+        container.removeChild(notifi);
+    }, 3000);
+}
+(_c = document.getElementById('fournisseur')) === null || _c === void 0 ? void 0 : _c.addEventListener('change', () => {
+    const selectElement = document.getElementById('fournisseur');
+    const transactionTitles = document.querySelectorAll('.transaction-title');
+    const selectedFournisseur = selectElement.value;
+    transactionTitles.forEach(transactionTitle => {
+        transactionTitle.classList.remove('om', 'wave', 'wari', 'cb');
+        if (selectedFournisseur === 'om') {
+            transactionTitle.classList.add('om');
+        }
+        else if (selectedFournisseur === 'wave') {
+            transactionTitle.classList.add('wave');
+        }
+        else if (selectedFournisseur === 'wari') {
+            transactionTitle.classList.add('wari');
+        }
+        else if (selectedFournisseur === 'cb') {
+            transactionTitle.classList.add('cb');
+        }
+    });
 });
--- Front_Typescript/src/script.ts
+++ Front_Typescript/src/script.ts
@@ -1,11 +1,104 @@
-document.getElementById('fournisseur')?.addEventListener('change', () => {
+
+
+
+function transfert() {
+    const montant = (<HTMLInputElement>document.getElementById('montant')).value;
+    const expediteur = (<HTMLInputElement>document.getElementById('expediteur')).value;
+    const destinataire = (<HTMLInputElement>document.getElementById('destinataire')).value;
+
+    const data = {
+        montant: montant,
+        numero_emetteur: expediteur,
+        numero_beneficiaire: destinataire,
+    };
+
+    fetch('http://127.0.0.1:8000/api/transferts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then((result: any) => {
+            console.log(result.message);
+            if (result.codeRetrait) {
+                console.log('Code de retrait: ' + result.codeRetrait);
+            }
+
+            if (result.emetteur) {
+                (<HTMLInputElement>document.getElementById('expediteur_nom')).value = result.emetteur.nom;
+            }
+
+            if (result.beneficiaire) {
+                (<HTMLInputElement>document.getElementById('destinataire_nom')).value = result.beneficiaire.nom;
+            }
+            if (result.message.includes('Transfert effectué avec succès.')) {
+                const message = `Le transfert effectué par ${result.emetteur?.nom} vers ${result.beneficiaire?.nom} est un succès.`;
+                notification(message);
+              }
+        })
+        .catch(error => console.error('Erreur lors du transfert:', error));
+}
+
+const boutonValider = document.querySelector('.btn-primary');
+boutonValider?.addEventListener('click', transfert);
+
+document.getElementById('expediteur')?.addEventListener('change', () => {
+    const expediteur = (<HTMLInputElement>document.getElementById('expediteur')).value;
+    fetch('http://127.0.0.1:8000/api/getclient/' + expediteur)
+        .then(response => response.json())
+        .then((result: any) => {
+            if (result.nom) {
+                (<HTMLInputElement>document.getElementById('expediteur_nom')).value = result.nom;
+            } else {
+                (<HTMLInputElement>document.getElementById('expediteur_nom')).value = '';
+            }
+        })
+        .catch(error => console.error('Erreur lors de la récupération du nom de l\'expéditeur:', error));
+});
+
+document.getElementById('destinataire')?.addEventListener('change', () => {
+    const destinataire = (<HTMLInputElement>document.getElementById('destinataire')).value;
+    fetch('http://127.0.0.1:8000/api/getclient/' + destinataire)
+        .then(response => response.json())
+        .then((result: any) => {
+            if (result.nom) {
+                (<HTMLInputElement>document.getElementById('destinataire_nom')).value = result.nom;
+            } else {
+                (<HTMLInputElement>document.getElementById('destinataire_nom')).value = '';
+            }
+        })
+        .catch(error => console.error('Erreur lors de la récupération du nom du destinataire:', error));
+});
+
+function showNotification(message: string) {
+    console.log('showNotification function called.');
+    if ('Notification' in window && Notification.permission === 'granted') {
+      const notification = new Notification(message);
+    }
+  }
+  
+  let container=document.querySelector('.container')
+  function notification(text: string) {
+    let notifi = document.createElement("div");
+    notifi.textContent = text;
+    notifi.classList.add("notification");
+    container.appendChild(notifi);
+  
+    setTimeout(() => {
+      container.removeChild(notifi);
+    }, 3000);
+  }
+
+  document.getElementById('fournisseur')?.addEventListener('change', () => {
     const selectElement = document.getElementById('fournisseur') as HTMLSelectElement;
     const transactionTitles = document.querySelectorAll('.transaction-title') as NodeListOf<HTMLHeadingElement>;
     const selectedFournisseur = selectElement.value;
 
     transactionTitles.forEach(transactionTitle => {
         transactionTitle.classList.remove('om', 'wave', 'wari', 'cb');
-        
+
         if (selectedFournisseur === 'om') {
             transactionTitle.classList.add('om');
         } else if (selectedFournisseur === 'wave') {
@@ -17,73 +110,3 @@ document.getElementById('fournisseur')?.addEventListener('change', () => {
         }
     });
 });
-
-
-
-function transfert() {
-    const montant = (<HTMLInputElement>document.getElementById('montant')).value;
-    const expediteur = (<HTMLInputElement>document.getElementById('expediteur')).value;
-    const destinataire = (<HTMLInputElement>document.getElementById('destinataire')).value;
-  
-    const data = {
-      montant: montant,
-      numero_emetteur: expediteur,
-      numero_beneficiaire: destinataire,
-    };
-  
-    fetch('http://127.0.0.1:8000/api/transferts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-      .then(response => response.json())
-      .then((result: any) => {
-        console.log(result.message);
-        if (result.codeRetrait) {
-          console.log('Code de retrait: ' + result.codeRetrait);
-        }
-  
-        if (result.emetteur) {
-          (<HTMLInputElement>document.getElementById('expediteur_nom')).value = result.emetteur.nom;
-        }
-  
-        if (result.beneficiaire) {
-          (<HTMLInputElement>document.getElementById('destinataire_nom')).value = result.beneficiaire.nom;
-        }
-      })
-      .catch(error => console.error('Erreur lors du transfert:', error));
-  }
-  
-  const boutonValider = document.querySelector('.btn-primary');
-  boutonValider?.addEventListener('click', transfert);
-  
-  document.getElementById('expediteur')?.addEventListener('change', () => {
-    const expediteur = (<HTMLInputElement>document.getElementById('expediteur')).value;
-    fetch('http://127.0.0.1:8000/api/getclient/' + expediteur)
-      .then(response => response.json())
-      .then((result: any) => {
-        if (result.nom) {
-          (<HTMLInputElement>document.getElementById('expediteur_nom')).value = result.nom;
-        } else {
-          (<HTMLInputElement>document.getElementById('expediteur_nom')).value = '';
-        }
-      })
-      .catch(error => console.error('Erreur lors de la récupération du nom de l\'expéditeur:', error));
-  });
-  
-  document.getElementById('destinataire')?.addEventListener('change', () => {
-    const destinataire = (<HTMLInputElement>document.getElementById('destinataire')).value;
-    fetch('http://127.0.0.1:8000/api/getclient/' + destinataire)
-      .then(response => response.json())
-      .then((result: any) => {
-        if (result.nom) {
-          (<HTMLInputElement>document.getElementById('destinataire_nom')).value = result.nom;
-        } else {
-          (<HTMLInputElement>document.getElementById('destinataire_nom')).value = '';
-        }
-      })
-      .catch(error => console.error('Erreur lors de la récupération du nom du destinataire:', error));
-  });
-  
